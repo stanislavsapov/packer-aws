ed7d3df0118ec3a49c06ba1d9b1c56b83853d79f
ssapov <stanislav.sapov@n26.com>
2020-10-23 11:23:28 +0200

    Add-validate

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,8 +18,19 @@ if [[ ! -f "$INPUT_VARIABLESFILE" ]] && [[ $INPUT_VARFILE -ne "*.json" ]]; then
 fi
 
 set +e
+# Run packer validate
+VALIDATE_OUTPUT=$(sh -c "packer validate -color=false -var-file=${INPUT_VARIABLESFILE} ${INPUT_TEMPLATEFILE}" 2>&1)
+VALIDATE_SUCCESS=$?
+echo "$VALIDATE_OUTPUT"
+
+if [[VALIDATE_SUCCESS -ne 0]]; then
+    echo "Packer validate has falied"
+    exit 1
+fi
+
+set +e
 # Run packer build
-BUILD_OUTPUT=$(sh -c "packer build -var-file=${INPUT_VARIABLESFILE} ${INPUT_TEMPLATEFILE}" 2>&1)
+BUILD_OUTPUT=$(sh -c "packer build -color=false -var-file=${INPUT_VARIABLESFILE} ${INPUT_TEMPLATEFILE}" 2>&1)
 BUILD_SUCCESS=$?
 echo "$BUILD_OUTPUT"
 set -e
